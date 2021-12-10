30bde7c9a7aebfcb4e401375f66e6135089b5df0
shreyasunhale0110 <shreyasunhale0110@gmail.com>
2021-12-10 11:10:35 +0530

    Added Basic Header and footer in Trackprogess HTML page

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -8,7 +8,7 @@ public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
-		helloWorld();
+		
 		
 	}
 	
