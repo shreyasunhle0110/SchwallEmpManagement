2b01f98c9151b54269d167d1a493a201597fdde5
shreyasunhale0110 <shreyasunhale0110@gmail.com>
2021-12-16 13:32:24 +0530

    Changed Column Names in registration Model

--- SEM_Backend-dev/src/main/java/net/javaguides/springboot/model/Employee.java
+++ SEM_Backend-dev/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -13,99 +13,40 @@ import lombok.Data;
 @Entity   //jpa entity
 @Table(name="employees")
 public class Employee {
-	
+
 	@Id  //denotes primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	//@generated value generation strategies for the values of primary keys
 	private Long id;
-	
-	@Column(name = "first_name", nullable = false)
-	private String firstName;
-	
-	@Column(name = "last_name")
-	private String lastName;
-	
-	@Column(name = "email")
+
+	@Column(name = "empName", nullable = false)
+	private String empName;
+
+
+	@Column(name = "empEmail")
 	private String email;
-	
-	@Column(name = "number")
+
+	@Column(name = "empNumber")
 	private Long number;
-	    
-     @Column(name = "address")
-	 private String address;
-	    
-	 @Column(name="role")
-	 private String role;
-	    
-	 @Column(name="doj")
-	 private String doj;
-	    
-	 @Column(name="dob")
-	 private String dob;
-	 
-	 @Column(name="password")
-	 private String password;
-	 
-	 
-	 
-	    
-	//getters and setters for password
-	 public String getPassword() {
-		return password;
-	}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	@Column(name = "empAddress")
+	private String address;
 
-		//getters and setters for dob
-	    public String getDob() {
-			return dob;
-		}
-
-		public void setDob(String dob) {
-			this.dob = dob;
-		}
-
-		//generate getters and setters for doj
-		public String getDoj() {
-			return doj;
-		}
-
-		public void setDoj(String doj) {
-			this.doj = doj;
-		}
-
-		//getters and setters for role
-	    public String getRole() {
-			return role;
-		}
-
-		public void setRole(String role) {
-			this.role = role;
-		}
-
-	//getters and setters for address
-	public String getAddress() {
-			return address;
-		}
-
-		public void setAddress(String address) {
-			this.address = address;
-		}
-
-		//getters and setters for number
-		public Long getNumber() {
-			return number;
-		}
-
-		public void setNumber(Long number) {
-			this.number = number;
-		}
+	@Column(name="roleID")
+	private Long roleID;
 	
 	
-	
-	
+
+	@Column(name="doj")
+	private String doj;
+
+	@Column(name="dob")
+	private String dob;
+
+	@Column(name="password")
+	private String password;
+
+
 
 	public Long getId() {
 		return id;
@@ -115,20 +56,12 @@ public class Employee {
 		this.id = id;
 	}
 
-	public String getFirstName() {
-		return firstName;
+	public String getEmpName() {
+		return empName;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setEmpName(String empName) {
+		this.empName = empName;
 	}
 
 	public String getEmail() {
@@ -139,22 +72,72 @@ public class Employee {
 		this.email = email;
 	}
 
+	public Long getNumber() {
+		return number;
+	}
+
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Long getRoleID() {
+		return roleID;
+	}
+
+	public void setRoleID(Long roleID) {
+		this.roleID = roleID;
+	}
+
+	public String getDoj() {
+		return doj;
+	}
+
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Employee(Long id, String empName, String email, Long number, String address, Long roleID, String doj,
+			String dob, String password) {
+		super();
+		this.id = id;
+		this.empName = empName;
+		this.email = email;
+		this.number = number;
+		this.address = address;
+		this.roleID = roleID;
+		this.doj = doj;
+		this.dob = dob;
+		this.password = password;
+	}
+
 	public Employee() {
 		super();
 	}
+	
 
-	public Employee(Long id, String firstName, String lastName, String email, Long number, String address, String role, String doj, String dob, String password) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.number=number;
-		this.address=address;
-		this.role= role;
-		this.doj=doj;
-		this.dob=dob;
-		this.password=password;
-	}
+
 
 }
